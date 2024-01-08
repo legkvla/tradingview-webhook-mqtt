14d2107d70f2a15bb785f819302603bcf1988956
Vladimir Legkunets <vladimir.legkunets@gmail.com>
2024-01-08 19:35:41 +0000

    Installing rclient as service

--- install-service.sh
+++ install-service.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-pip3 install -r requirements.txt -t deps
+pip3 install -r client-requirements.txt -t deps
 cp tvrclient.plist ~/Library/LaunchAgents/
 launchctl load ~/Library/LaunchAgents/tvrclient.plist
--- tv-events-client.sh
+++ tv-events-client.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 /opt/homebrew/bin/heroku config:get REDIS_TLS_URL -s > .env
 /opt/homebrew/bin/heroku config:get SEC_KEY -s >> .env
-python3 rclient.py
+/usr/bin/python3 -u rclient.py
