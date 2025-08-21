960f5c35b674f3eb276e5422f955a15c75a34bf9
oleglegkunets <112647976+oleglegkunets@users.noreply.github.com>
2025-08-21 17:21:56 +0200

    Testing HTTP connection added + connection timer and looping connection

--- redisConnectionTest.py
+++ redisConnectionTest.py
@@ -15,14 +15,19 @@ payload = json.dumps({
 headers = {
   'Content-Type': 'application/json'
 }
-i = 1;
+total_elapsed = 0
+average_elapsed = 0
+i = 1
 while i > 0:
   start_time = time.perf_counter()
   response = requests.post(url, headers=headers, data=payload, timeout=15)
   elapsed_ms = (time.perf_counter() - start_time) * 1000.0
+  total_elapsed += elapsed_ms
+  average_elapsed = total_elapsed / i
 
   print(f"HTTP {response.status_code} in {elapsed_ms:.2f} ms")
   print(response.text)
+  print(f"Average HTTP success in : {average_elapsed:.2f}")
   i = i + 1
   if response.status_code != 200:
     break
